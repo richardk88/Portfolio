9cb46940b2cfef70e28d2ca093a85beb527291bf
richardk88 <richardk88@hotmail.com>
2017-09-02 18:56:50 -0400

    created home component and install styled-components and icons

---
+++ src/components/Home.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+import { Link, Redirect } from 'react-router-dom';
+
+
+class Home extends Component {
+    render() {
+        return (
+            <div>
+                hello
+            </div>
+        );
+    }
+}
+
+export default Home;
