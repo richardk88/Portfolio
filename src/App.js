c4989fcea6b18d95ce1bc197787b714e8314eb93
richardk88 <richardk88@hotmail.com>
2017-09-04 19:53:26 -0400

    set main nav to Home component

--- src/App.js
+++ src/App.js
@@ -5,23 +5,22 @@ import MdIconPack, {MdMenu} from 'react-icons/lib/md'
 import {FaBars} from 'react-icons/lib/fa'
 import styled from 'styled-components';
 import Home from './components/Home';
-import Test from './components/Test';
 
-const Nav = styled.div`
-  background-color: rgba(0,0,0,0);
-  position: fixed;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  padding: 55px 5vw;
-  width: 100%;
-  height: 0px;
-    div{
-      font-size: 2.55rem;
-      font-family: 'Libre Franklin', sans-serif;
-      margin: 0;
-      svg{ margin-bottom: 5px; }
-}`
+// const Nav = styled.div`
+//   background-color: rgba(0,0,0,0);
+//   position: fixed;
+//   display: flex;
+//   justify-content: space-between;
+//   align-items: center;
+//   padding: 55px 5vw;
+//   width: 100%;
+//   height: 0px;
+//     div{
+//       font-size: 2.55rem;
+//       font-family: 'Libre Franklin', sans-serif;
+//       margin: 0;
+//       svg{ margin-bottom: 5px; }
+// }`
 
 const Footer = styled.p`
   background-color: rgba(0,0,0,.55);
@@ -48,23 +47,21 @@ class App extends Component {
       <Router>
         <div>
 
-        <Nav>
-        <div><Link to="/"><img src='https://i.imgur.com/UJC52lF.png' className='logo-style'/></Link></div>
-          <div></div>
-          <div className='menu-container'>
-            <span className='menu-text'>Menu</span>
-            <Link to="/" className='menu-bar'><MdMenu /></Link>
-          </div>
-        </Nav>
+          {/* <Nav>
+          <div><Link to="/"><img src='https://i.imgur.com/UJC52lF.png' className='logo-style'/></Link></div>
+            <div></div>
+            <div className='menu-container'>
+              <span className='menu-text'>Menu</span>
+              <Link to="/" className='menu-bar'><MdMenu /></Link>
+            </div>
+          </Nav> */}
 
-        
+          <Home />
           
           <Footer>
             <p>© 2017 by Richard Kim</p>
           </Footer>
-
-          <Route exact path="/" component={Home} /> 
-          <Route exact path="/test" component={Test} /> 
+ 
         </div>
       </Router>
     );
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,39 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
 import { Link, Redirect } from 'react-router-dom';
+import MdIconPack, {MdMenu} from 'react-icons/lib/md'
 
+const Nav = styled.div`
+    background-color: rgba(0,0,0,0);
+    position: absolute;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 55px 5vw;
+    width: 100%;
+    height: 0px;
+    div{
+        font-size: 2.55rem;
+        font-family: 'Libre Franklin', sans-serif;
+        margin: 0;
+        svg{ margin-bottom: 5px; }
+}`
 
 class Home extends Component {
     render() {
         return (
-            <div id="background">
-                <img src='https://i.imgur.com/T9YYqKP.png' className='main_img'/>
+            <div>
+                <Nav>
+                <   div><Link to="/"><img src='https://i.imgur.com/UJC52lF.png' className='logo-style'/></Link></div>
+                    <div></div>
+                    <div className='menu-container'>
+                        <span className='menu-text'>Menu</span>
+                        <Link to="/" className='menu-bar'><MdMenu /></Link>
+                    </div>
+                </Nav>
+                <div id="background">
+                    <img src='https://i.imgur.com/T9YYqKP.png?2' className='home_img'/>
+                </div>
             </div>
         );
     }
