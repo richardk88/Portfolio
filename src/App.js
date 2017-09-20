e2ce83465747c75fe958bd2cfaaca5779ef8bcd0
richardk88 <richardk88@hotmail.com>
2017-09-20 12:11:17 -0400

    created footer component

--- src/App.js
+++ src/App.js
@@ -1,39 +1,19 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
-import MdIconPack, {MdMenu} from 'react-icons/lib/md'
-import {FaBars} from 'react-icons/lib/fa'
-import styled from 'styled-components';
 import NavBar from './components/NavBar';
 import Home from './components/Home';
-
-const Footer = styled.p`
-  background-color: rgba(0,0,0,.60);
-
-  bottom: 0px;
-  width: 100vw;
-  height: .5vw;
-  text-align: center;
-  margin-bottom: 0;
-  padding: 30px 1vw;
-    p{
-      color: white;
-      font-size: 1rem;
-      margin: 0;
-      svg{ margin-bottom: 5px; }
-}`
+import Footer from './components/Footer';
 
 class App extends Component {
   render() {
     return (
       <Router>
         <div>
+
           <NavBar />
           <Home />
+          <Footer />
           
-          <Footer staticBottom>
-            <p>© <span className='footer-year'>201</span>7 Richard Kim. All rights reserved.</p>
-          </Footer>
- 
         </div>
       </Router>
     );
---
+++ src/components/Footer.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Style = styled.p`
+    background-color: #151515;
+    bottom: 0px;
+    width: 100vw;
+    height: .5vw;
+    text-align: center;
+    margin-bottom: 0;
+    padding: 30px 1vw;
+    p{
+        color: #696969;
+        font-size: 1rem;
+        margin: 0;
+        svg{ margin-bottom: 5px; }
+    }
+`
+
+const Footer = () => {
+    return (
+        <Style staticBottom>
+            <p>© 2017 Richard Kim. All rights reserved.</p>
+        </Style>
+    );
+};
+
+export default Footer;
