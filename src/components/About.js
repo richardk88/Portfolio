58f75fccaad993deb889ca82597ba6bc136c5fe1
richardk88 <richardk88@hotmail.com>
2017-09-22 00:41:19 -0400

    trying to wrap skills

--- src/components/About.js
+++ src/components/About.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 class About extends Component {
+
     render() {
         return (
             <div>
@@ -11,6 +12,29 @@ class About extends Component {
                 </div>
                 <div className='about'>
                     <h1>A<span className='underline'>BOU</span>T</h1>
+        
+                    <div className='aboutContainer'>
+                        <div className='aboutMe'>
+                            <img src={require('./images/headshot.png')} className='headshot' />
+                            <p>
+                                I am a Full-Stack Web Developer based in Atlanta, GA. With experience in building richer interactive web apps I maintain a healthy balance between functionality and visual impact in all of my work. 
+                                Feel free to look at my portfolio and don't hesitate to contact me if you think I can be of service to you. 
+                                Thanks for stopping by!
+                            </p>
+                        </div>
+                        <div className='skills'>
+                            <div className='skill'> 
+                                <img src={require('./images/html5.png')} alt='html5'/>
+                                <img src={require('./images/css3.png')} alt='css3'/>
+                                <img src={require('./images/react.png')} alt='react'/>
+                                <img src={require('./images/ruby.png')} alt='ruby'/>
+                                <img src={require('./images/rails.png')} alt='rails'/>
+                                <img src={require('./images/javascript.png')} alt='javascript'/>
+                                <img src={require('./images/github.png')} alt='github'/>
+                                <img src={require('./images/heroku.png')} alt='heroku'/>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         );
