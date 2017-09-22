5818cfd9b546b2a0e5c8446441890e6d3e8aaa64
richardk88 <richardk88@hotmail.com>
2017-09-22 13:14:24 -0400

    need to work on projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -5,8 +5,14 @@ class Projects extends Component {
         return (
             <div className='projects' id='projects'>
                 <h1 className='projectsTitle'>P<span className='underline'>ROJECT</span>S</h1>
-                <div className='projectContainer'>
-                    <a><div></div></a>
+                <div container>
+                    <div className='projectContainer'>
+                        <a href='http://photographer-watches-86485.bitballoon.com/'><img src={require('./images/hangman.png')} className='projectImage'/></a>
+                        <a href='https://mysterious-waters-71092.herokuapp.com/'><img src={require('./images/bucket_by_day.png')} className='projectImage'/></a>
+                        <a href='https://make-it-yourself.herokuapp.com/'><img src={require('./images/miy.png')} className='projectImage'/></a>
+                        <a href='http://project-rover.herokuapp.com/'><img src={require('./images/rovr.png')} className='projectImage'/></a>
+                        <a href='https://nomnom-app.herokuapp.com/'><img src={require('./images/nomnom.png')} className='projectImage'/></a>
+                    </div>
                 </div>
             </div>
         );
