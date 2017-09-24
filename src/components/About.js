5811bd60cc000e35c7bac23e4e27e56cc9172271
richardk88 <richardk88@hotmail.com>
2017-09-23 22:36:21 -0400

    added transition image to about

--- src/components/About.js
+++ src/components/About.js
@@ -19,23 +19,24 @@ class About extends Component {
                             <img src={require('./images/headshot.png')} className='headshot' />
                             <p className='paragraph'>
                                 Hi! I'm an Atlanta based Full-Stack Web Developer focused on creating clean, responsive web apps by maintaining a healthy balance between functionality and a richer user experience in all of my work. 
-                                Whether you're interested in working with me or just want to say hello, I'd love to hear from you!
+                                Whether you're interested in working with me or just want to say hello, <a href='#contact' className='contactMe'>I'd love to hear from you</a>!
                             </p>
                         </div>
                         <div className='skills'>
                             <div className='skill'> 
-                                <img src={require('./images/html5.png')} alt='html5'/>
-                                <img src={require('./images/css3.png')} alt='css3'/>
-                                <img src={require('./images/react.png')} alt='react'/>
-                                <img src={require('./images/ruby.png')} alt='ruby'/>
-                                <img src={require('./images/rails.png')} alt='rails'/>
-                                <img src={require('./images/javascript.png')} alt='javascript'/>
-                                <img src={require('./images/node-js.png')} alt='node'/>
-                                <img src={require('./images/mongodb.png')} alt='mongodb'/>
+                                <img src={require('./images/html5.png')} alt='html5' className='hvr-grow'/>
+                                <img src={require('./images/css3.png')} alt='css3' className='hvr-grow'/>
+                                <img src={require('./images/react.png')} alt='react' className='hvr-grow'/>
+                                <img src={require('./images/ruby.png')} alt='ruby' className='hvr-grow'/>
+                                <img src={require('./images/rails.png')} alt='rails' className='hvr-grow'/>
+                                <img src={require('./images/javascript.png')} alt='javascript' className='hvr-grow'/>
+                                <img src={require('./images/node-js.png')} alt='node' className='hvr-grow'/>
+                                <img src={require('./images/mongodb.png')} alt='mongodb' className='hvr-grow'/>
                             </div>
                         </div>
                     </div>
                 </div>
+                <img src={require('./images/transition.png')} className='transition'/>
             </div>
         );
     }
