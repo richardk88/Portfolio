399b31a5d7a12351cca6c747334309073ac9ab14
richardk88 <richardk88@hotmail.com>
2017-09-23 20:39:34 -0400

    added hover effect to nav

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -9,16 +9,16 @@ class NavBar extends Component {
                 <Navbar inverse fixedTop collapseOnSelect className='navbar-container'>
                     <Navbar.Header>
                         <Navbar.Brand>
-                            <a href="#home"><Image src='https://i.imgur.com/UJC52lF.png' className='logo-img' responsive/></a>                
+                            <a href="#home"><Image src='https://i.imgur.com/UJC52lF.png' className='logo-img hvr-grow' responsive/></a>                
                         </Navbar.Brand>
                     <Navbar.Toggle />
                     </Navbar.Header>
                     <Navbar.Collapse>
                         <Nav pullRight>
-                            <a href="#home"><NavItem eventKey={1} href="#home">Home</NavItem></a>
-                            <a href="#about"><NavItem eventKey={2} href="#about">About</NavItem></a>
-                            <a href="#projects"><NavItem eventKey={3} href="#projects">Projects</NavItem></a>
-                            <a href="#contact"><NavItem eventKey={4} href="#contact"><button className='contactBtn'>Contact</button></NavItem></a>
+                            <a href="#home"><NavItem eventKey={1} href="#home" className='hvr-grow'>Home</NavItem></a>
+                            <a href="#about"><NavItem eventKey={2} href="#about" className='hvr-grow'>About</NavItem></a>
+                            <a href="#projects"><NavItem eventKey={3} href="#projects" className='hvr-grow'>Projects</NavItem></a>
+                            <a href="#contact"><NavItem eventKey={4} href="#contact"><button className='contactBtn hvr-grow'>Contact</button></NavItem></a>
                         </Nav>
                     </Navbar.Collapse>
                 </Navbar>
